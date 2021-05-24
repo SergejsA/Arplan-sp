d4aeea8b18faecc63e28a14c778a269afbdb6d8c
Eduards Spagis <code@esit.lv>
2021-05-24 11:44:43 +0300

    Cron test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('check:jobs')->cron('40 8 * * *');
+        $schedule->command('check:jobs')->cron('0 5 * * *');
     }
 
     /**
